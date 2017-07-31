ab695c7d6f31db1b3356701352ba1ebaaa579c24
a-shershen <accs.shershen@gmail.com>
2017-07-31 14:02:49 +0300

    TEST
    Create Test class.
    Create BubbleSort test.

---
+++ SortingLib.TEST/SorterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingLib.TEST
+{
+    [TestClass]
+    public class SorterTests
+    {
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            int[] unsortedArray = { 5, 1, 3, 2, 4, 4 };
+
+            int[] expected = { 1, 2, 3, 4, 4, 5 };
+
+            int[] actual = SortingLib.LIB.Sorter.BubbleSort(unsortedArray);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
